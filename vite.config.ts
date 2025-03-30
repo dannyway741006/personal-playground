64ff0297246c605bc879d9fbd54bf37ae9b64351
wilson <dannyway741006@gmail.com>
2025-03-30 14:00:11 +0800

    dist test

--- vite.config.ts
+++ vite.config.ts
@@ -1,74 +1,21 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
-// import Pages from "vite-plugin-pages"
-
-// import AutoImport from "unplugin-auto-import/vite"
-// import Components from "unplugin-vue-components/vite"
-import Layouts from "vite-plugin-vue-layouts"
 import Vuetify from "vite-plugin-vuetify"
 import glsl from "vite-plugin-glsl"
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: true, // 允许使用本地 IP 地址访问
-    allowedHosts: [
-      ".ngrok-free.app", // 通配符，允許所有 *.ngrok-free.app 子域名
-    ],
-    port: mode === "production" ? 3030 : 5174,
-  },
-  ssr: {
-    noExternal: mode === "development" ? ["vue-router"] : [],
-  },
+export default defineConfig({
   build: {
-    target: "es2022", // 或 'esnext'
-    outDir: "root",
+    outDir: "dist", // 確保輸出到 dist 資料夾
     assetsDir: "assets",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-      output: {
-        comments: false,
-      },
-      toplevel: true, //增加壓縮率
-    },
-    chunkSizeWarningLimit: 5000, //大於5MB警告
+    target: "es2022",
   },
   plugins: [
-    glsl(),
-    // AutoImport({
-    //   imports: ["vue", "vue-router"],
-    //   dts: true,
-    //   eslintrc: {
-    //     enabled: true,
-    //   },
-    //   vueTemplate: true,
-    // }),
-    // Components(),
-    // Pages({
-    //   dirs: "src/pages",
-    //   exclude: ["**/components/*.vue"],
-    //   extendRoute(route, parent) {
-    //     if (route.path === "/")
-    //       return {
-    //         ...route,
-    //         // redirect: "login",
-    //       }
-    //   },
-    //   importMode: "async",
-    // }),
-    // Layouts({
-    //   layoutsDirs: "src/layouts",
-    //   defaultLayout: "deshboard",
-    // }),
-
     vue(),
     Vuetify({
       autoImport: true,
-      styles: {
-        configFile: "src/styles/settings.sass",
-      },
+      styles: { configFile: "src/styles/settings.sass" },
     }),
+    glsl(),
   ],
-  base: "/personal-playground/", // 必須與你的倉庫名稱一致
-}))
+  base: "/personal-playground/", // 與你的 GitHub Pages 路徑一致
+})
